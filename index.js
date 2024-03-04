f175b9f83c99976ce2d0d2a570cfa952391df33e
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2024-03-04 13:34:32 +0600

    skeleton

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// Import the Express module
 const express = require('express');
-
-// Create an instance of Express
+const fs = require('fs');
 const app = express();
 
-// Define a route for the root URL ("/")
-app.get('/', (req, res) => {
-  res.send('Hello World!dw'); // Send "Hello World!" as the response
+const badwordlist = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+
+app.get('/:name', (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const isInList = badwordlist.includes(name);
+  if (isInList) {
+    res.json({ word: `${name}`, is_badword: true});
+  } else {
+    res.json({ word: `${name}`, is_badword: false });
+  }
 });
 
-// Start the server on port 3000
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
