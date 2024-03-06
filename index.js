e2b06e2b54b629c0e7269ae970948464e4041d4b
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2024-03-06 23:43:05 +0600

    cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 
 // Specify allowed origin
-const allowedOrigins = ['http://127.0.0.1:5500'];
+const allowedOrigins = ['https://sigmakib2.github.io/Nirmol/'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
